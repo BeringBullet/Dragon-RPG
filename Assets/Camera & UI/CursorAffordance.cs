f3c0c5f5eede8bd87e204330c1e7ce132ed5b9e5
Mike Walters <mike@beringbullet.com>
2019-01-18 15:08:34 -0600

    added corsor

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField] Texture2D walkableSFX;
+    [SerializeField] Texture2D unknownSFX;
+    [SerializeField] Texture2D TargetSFX;
+
+    CameraRaycaster cameraRaycaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkableSFX, Vector2.zero, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(TargetSFX, Vector2.zero, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownSFX, Vector2.zero, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(unknownSFX, Vector2.zero, CursorMode.Auto);
+                break;
+        }
+    }
+}
